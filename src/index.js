b60143e2ada70ac78266769666be56c6d7960044
patrycjamilo1 <patrycjamilo1@gmail.com>
2024-05-06 12:38:47 +0200

    feat(hosting): use security needed packages before deployment

--- src/index.js
+++ src/index.js
@@ -5,10 +5,21 @@ const cors = require('cors');
 const swaggerJsdoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
 const logsRoutes = require('./routes/logs.routes');
+const compression = require('compression');
+const helmet = require('helmet');
+const RateLimit = require("express-rate-limit");
+
 
 const app = express();
-app.use(cors())
+app.use(cors());
+app.use(helmet());
 
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
+
+app.use(limiter);
 // Swagger config
 const options = {
   definition: {
@@ -46,6 +57,7 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   })
   .catch(err => console.error('Database connection error:', err));
 
+  app.use(compression())
 // Register routes
 app.use('/logs', logsRoutes);
 
--- src/services/logs.service.js
+++ src/services/logs.service.js
@@ -3,6 +3,7 @@ const logsModel = require('../db/models/logs.model');
 
 class LogsService {
     static async getLogs(limit, skip, filters) {
+      console.log(await logsModel.countDocuments());
         const totalRows = await logsModel.countDocuments(filters);
         const totalPages = Math.ceil(totalRows / limit);
         const logs = await logsModel.find(filters)
